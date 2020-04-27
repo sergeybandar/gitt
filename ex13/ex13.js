076ba6aaa620f8897ab3e729cb8090b576bc3a0c
sergeybandar <sergeybandar@mail.ru>
2020-04-28 02:20:44 +0300

    exercise13 small changes

--- ex13/ex13.js
+++ ex13/ex13.js
@@ -2,9 +2,6 @@ const but = document.getElementById('but');
 const R = 6400;
 const table = document.getElementById('table');
 let coords, coordsAPI;
-
-
-
 getPosition()
     .then(function (coords) {
         this.coords = {
@@ -24,16 +21,13 @@ getPosition()
     .catch(function (err) {
         console.error(err);
     })
-
 function getDistance(a, b) {
-
     const h = Math.sin((Math.PI / 180) * (b.latitude - a.latitude) / 2) ** 2 + Math.cos((Math.PI / 180) * a.latitude) * Math.cos((Math.PI / 180) * b.latitude) * (Math.sin((Math.PI / 180) * (b.longitude - a.longitude) / 2) ** 2);
     const distance = 2 * R * Math.asin(Math.sqrt(h)) * 1000;
     const tr = document.createElement('tr');
-    tr.innerHTML = `${distance}`;
+    tr.innerHTML = `<td colspan="2">Расстояние между координатами</td><td colspan="2">${distance.toFixed(3)}</td>`;
     table.appendChild(tr);
 }
-
 function getPosition() {
     return new Promise(function (result, reject) {
 
@@ -43,40 +37,28 @@ function getPosition() {
 
             reject(err);
         })
-
     })
 }
-
 function getPositionAPI() {
     return new Promise(function (result, reject) {
-
         const xhr = new XMLHttpRequest();
         xhr.open('GET', `https://freegeoip.app/json/`, true);
         xhr.onload = function () {
             result(JSON.parse(this.responseText));
-
         }
         xhr.onerror = () => reject('Error!');
-
         xhr.send(null);
     })
-
-
 }
-
-
 const tbWeather = document.getElementById('tableWeather');
 getPosition().then(function (coords) {
     const url = `https://www.metaweather.com/api/location/search/?lattlong=${coords.latitude},${coords.longitude}`;
-
-
     return getFile(url);
 })
     .then(async function (cities) {
 
         for (let i = 0; i < 5; i++) {
             const citiWeather = await getFile(`https://www.metaweather.com/api/location/${cities[i].woeid}/`);
-
             const tr = document.createElement('tr');
             tr.innerHTML = `
             <td>${citiWeather.title}</td>
@@ -86,18 +68,15 @@ getPosition().then(function (coords) {
             <td>${Math.round(citiWeather.consolidated_weather[0].humidity)}</td>`;
             tbWeather.appendChild(tr);
         }
-
     })
     .catch(function (err) {
         console.error(err);
     })
-
 function getTr(coords, coordsAPI) {
     const tr = document.createElement('tr');
-    tr.innerHTML = `<td>${coords.latitude}</td><td>${coords.longitude}</td><td>${coordsAPI.latitude}</td><td>${coordsAPI.longitude}</td>`;
+    tr.innerHTML = `<td>${coords.latitude.toFixed(2)}</td><td>${coords.longitude.toFixed(2)}</td><td>${coordsAPI.latitude.toFixed(2)}</td><td>${coordsAPI.longitude.toFixed(2)}</td>`;
     table.appendChild(tr);
 }
-
 function getFile(url) {
     return new Promise(function (result, reject) {
 
@@ -105,12 +84,8 @@ function getFile(url) {
         xhr.open('GET', `${url}`, true);
         xhr.onload = function () {
             result(JSON.parse(this.responseText));
-
         }
         xhr.onerror = () => reject('Error!');
-
         xhr.send(null);
     })
-
-
 }
